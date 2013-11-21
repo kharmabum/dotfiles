07290f008e489500ccf49515ffb7a9f6cb1f1050
Juan-Carlos Foust <jc@fototropik.com>
2013-11-21 15:29:50 -0800

    updated PATH for anaconda. typo in pubkey hack.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,10 +9,10 @@ alias projects='cd $PROJECTS'
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
   alias l="gls -lAh --color"
+  alias la="gls -A --color"
   alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="gls -F --color"
 fi
 
 # taken from @paulmllr dotfiles
