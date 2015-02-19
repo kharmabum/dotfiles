58a1b7b22c6d5723753aeb4c59f82fd765c45125
Juan-Carlos Foust <jc@fototropik.com>
2015-02-18 16:10:10 -0800

    * stuff

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,7 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:./bin:$HOME/.rbenv/shims:/usr/local/opt/ruby/bin:/usr/local/heroku/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NODE_PATH='/usr/local/lib/node_modules'
+
+export DOCKER_HOST='tcp://192.168.59.103:2376'
+export DOCKER_CERT_PATH='/Users/jcfoust/.boot2docker/certs/boot2docker-vm'
+export DOCKER_TLS_VERIFY='1'
