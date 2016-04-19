b942bd51d67071ab9863d42dfdf15cd92eca7740
Juan-Carlos Foust <jc@fototropik.com>
2016-04-19 15:25:25 -0700

    derp

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,5 +1,5 @@
 # Docker
 export DOCKER_TLS_VERIFY="1"
 export DOCKER_HOST="tcp://192.168.99.100:2376"
-export DOCKER_CERT_PATH="/Users/IO/.docker/machine/machines/docker-vm"
-export DOCKER_MACHINE_NAME="docker-vm"
+export DOCKER_CERT_PATH="/Users/IO/.docker/machine/machines/default"
+export DOCKER_MACHINE_NAME="default"
