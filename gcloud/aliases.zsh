458e129ed05f9c30a3126123d210eac881cb91b7
Juan-Carlos Foust <jfoust@ginkgobioworks.com>
2024-07-09 08:55:28 -0600

    updates

--- gcloud/aliases.zsh
+++ gcloud/aliases.zsh
@@ -1,12 +1,10 @@
 
+export CLOUDSDK_PYTHON=$(pyenv prefix 3.11)/bin/python
+
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/jfoust/google-cloud-sdk/path.bash.inc' ]; then . '/Users/jfoust/google-cloud-sdk/path.bash.inc'; fi
+if [ -f '/usr/local/bin/google-cloud-sdk/path.zsh.inc' ]; then . '/usr/local/bin/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '/Users/jfoust/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/jfoust/google-cloud-sdk/completion.bash.inc'; fi
+if [ -f '/usr/local/bin/google-cloud-sdk/completion.zsh.inc' ]; then . '/usr/local/bin/google-cloud-sdk/completion.zsh.inc'; fi
 
 
-
-alias diglett-pg='cloud-sql-proxy diglett-a98cc:us-west1:diglett --port 6666'
-
-export CLOUDSDK_PYTHON=$(pyenv prefix 3.11)/bin/python
