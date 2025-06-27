8fae82d522df2bf2a14c2f0b9ae5381a3528f408
Juan-Carlos Foust <j_foust@apple.com>
2025-06-26 20:26:57 -0700

    updates

---
+++ bump/aliases.zsh
@@ -0,0 +1,6 @@
+alias bump-install='brew install bump-my-version'
+alias bump-show='bump-my-version show current_version'
+alias bump='bump-my-version bump' # {patch,minor,major}   
+alias bump-patch='bump-my-version bump patch'
+alias bump-minor='bump-my-version bump minor'
+alias bump-major='bump-my-version bump major'
--- php/aliases.zsh
+++ php/aliases.zsh
@@ -1,2 +1,3 @@
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+alias sailup='sail up -d --force-recreate'
 alias phpf='sail composer run phpcbf'
