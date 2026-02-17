b80b92c00a6cbac59e9add14a9238ecbe59d9834
Juan-Carlos Foust <j_foust@apple.com>
2026-02-17 11:45:13 -0800

    chore(node): remove Volta configuration and ignore local overrides
    Remove Volta-specific environment variables and default alias from `node/path.zsh`.
    Add `**/local.zsh` to `.gitignore` to support machine-specific configurations.

--- node/path.zsh
+++ node/path.zsh
@@ -2,11 +2,3 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-# Delegate to Volta by default
-nvm alias default system
-
-# Load Volta after NVM
-export VOLTA_FEATURE_PNPM=1
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
