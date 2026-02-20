73b748a361f81c72dcef2ac64151ef8a1ece197e
Juan-Carlos Foust <j_foust@apple.com>
2026-02-19 23:58:06 -0800

    feat(aliases): add diglett connectivity and streamline git workflow
    Add new `diglett/aliases.zsh` with SSH, VNC, and Chromium access commands for
    the diglett host. Introduce `gcp` alias in `git/aliases.zsh` to combine staging,
    committing, and pushing. Add `kairos-vnc` in `kairos/aliases.zsh` for VNC access.

---
+++ diglett/aliases.zsh
@@ -0,0 +1,6 @@
+alias diglett-ssh='ssh diglett'
+alias diglett-vnc='open -a "/Applications/TigerVNC.app" --args localhost:5901'
+alias diglett-vnc-tunnel='cloudflared access tcp --hostname vnc.diglett.xyz/ --url localhost:5901'
+
+# NUC
+alias diglett-chromium='google-chrome --disable-gpu &'
--- kairos/aliases.zsh
+++ kairos/aliases.zsh
@@ -1,1 +1,2 @@
 alias kairos='cd /Users/jfoust/Development/life/kairos' 
+alias kairos-vnc='open -a "/Applications/TigerVNC.app" --args localhost:5900'
